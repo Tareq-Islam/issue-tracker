90f6a1b773df7736602f5f67404ff9598f3ba835
Tareq Islam <tareqislam42@gmail.com>
2024-02-22 23:56:59 +0600

    [add] issues controller and  routes

---
+++ issue_tracker/API/Controllers/IssueController.cs
@@ -0,0 +1,97 @@
+﻿using API.Attributes;
+using Application.Features.IssueFeature;
+using Domain.Enums;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : BaseController
+    {       
+        [HttpGet]
+        public async Task<IActionResult> Gets()
+        {
+            var response = await _mediator.Send(new GetListQuery());
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("status")]
+        public async Task<IActionResult> GetStatus()
+        {
+            var response = await _mediator.Send(new GetListQuery());
+            return StatusCode(response.StatusCode, response);
+        }
+
+        //[HttpGet("{id}/track")]
+        //public async Task<IActionResult> GetTrack(int id)
+        //{
+        //    var response = await _mediator.Send(new GetListQuery());
+        //    return StatusCode(response.StatusCode, response);
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> Create([FromBody] CreateCommand dto)
+        //{
+        //    var response = await _mediator.Send(dto);
+        //    return StatusCode(response.StatusCode, response);
+        //}
+
+        //[HttpPost("{id}/comment")]
+        //public async Task<IActionResult> CreateComment([FromBody] CreateCommand dto)
+        //{
+        //    var response = await _mediator.Send(dto);
+        //    return StatusCode(response.StatusCode, response);
+        //}
+
+        //[HttpPut("{id}/close")]
+        //public async Task<IActionResult> Close(int id)
+        //{
+        //    var response = await _mediator.Send(new GetListQuery());
+        //    return StatusCode(response.StatusCode, response);
+        //}
+
+        //[HttpPut("{id}/suspend")]
+        //public async Task<IActionResult> Suspend(int id)
+        //{
+        //    var response = await _mediator.Send(new GetListQuery());
+        //    return StatusCode(response.StatusCode, response);
+        //}
+
+        //[HttpPut("{id}/causeFinding")]
+        //public async Task<IActionResult> UpdateCauseFindings(int id)
+        //{
+        //    var response = await _mediator.Send(new GetListQuery());
+        //    return StatusCode(response.StatusCode, response);
+        //}
+
+        //[HttpPut("{id}/solutionTag")]
+        //public async Task<IActionResult> UpdateSolutionTag(int id)
+        //{
+        //    var response = await _mediator.Send(new GetListQuery());
+        //    return StatusCode(response.StatusCode, response);
+        //}
+
+        //[HttpPut("{id}/Assignee")]
+        //public async Task<IActionResult> UpdateAssignees(int id)
+        //{
+        //    var response = await _mediator.Send(new GetListQuery());
+        //    return StatusCode(response.StatusCode, response);
+        //}
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id)
+        {
+            var response = await _mediator.Send(new GetListQuery());
+            return StatusCode(response.StatusCode, response);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _mediator.Send(new GetListQuery());
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
---
+++ issue_tracker/Application/Features/IssueFeature/GetListQuery.cs
@@ -0,0 +1,43 @@
+﻿using Application.Abstractions.Services;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Application.Features.IssueFeature
+{
+    public class GetListQuery : IQuery<IApiResult>
+    {
+        internal class GetListQueryHandler : IRequestHandler<GetListQuery, IApiResult>
+        {
+            private readonly IUnitOfWork _unitOfWork;
+            private readonly ICurrentUserService _currentUserService;
+
+            public GetListQueryHandler(IUnitOfWork unitOfWork, ICurrentUserService currentUserService)
+            {
+                _unitOfWork = unitOfWork;
+                _currentUserService = currentUserService;
+            }
+
+            public async Task<IApiResult> Handle(GetListQuery request, CancellationToken cancellationToken)
+            {
+
+                var user = _unitOfWork.User.Queryable.Where(x => x.IsDeleted == 0).Select(x => new {
+                    x.Id,
+                    x.Role.RoleName,
+                    x.Role.RoleType,
+                    VendorName = x.Vendor.Name,
+                    x.VendorId,
+                    x.UserName,
+                    x.RoleId,
+                    x.LoginName,
+                    x.UserEmail,
+                    x.UserMobileNumber,
+                }).AsQueryable();
+
+                var data = await user.ToListAsync();
+
+                return ApiResult<dynamic>.Success(data);
+            }
+        }
+    }
+
+}
