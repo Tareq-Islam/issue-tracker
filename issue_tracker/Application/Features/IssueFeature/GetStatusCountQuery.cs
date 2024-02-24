6623d50316cf10112cbd09c6a0308068659626e8
Tareq Islam <tareqislam42@gmail.com>
2024-02-24 19:30:27 +0600

    [fix] issues

--- issue_tracker/Application/Features/IssueFeature/GetStatusCountQuery.cs
+++ issue_tracker/Application/Features/IssueFeature/GetStatusCountQuery.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Abstractions.Services;
+using Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -9,22 +10,19 @@ namespace Application.Features.IssueFeature
         internal class GetIssueStatusCountQueryHandler : IRequestHandler<GetIssueStatusCountQuery, IApiResult>
         {
             private readonly IUnitOfWork _unitOfWork;
-            private readonly ICurrentUserService _currentUser;
-            public GetIssueStatusCountQueryHandler(IUnitOfWork unitOfWork, ICurrentUserService currentUser)
+            public GetIssueStatusCountQueryHandler(IUnitOfWork unitOfWork)
             {
                 _unitOfWork = unitOfWork;
-                _currentUser = currentUser;
             }
 
             public async Task<IApiResult> Handle(GetIssueStatusCountQuery request, CancellationToken cancellationToken)
             {
                 var items = _unitOfWork.Issue.Queryable.Where(x => x.IsDeleted == 0).AsQueryable();
                 var data = await items.ToListAsync();
-                int open = data.Where(x => x.Status == 1).Count();
-                int closed = data.Where(x => x.Status == 2).Count();
-                int suspend = data.Where(x => x.Status == 3).Count();
+                int open = data.Where(x => x.Status == (int)IssueStatusEnum.Open).Count();
+                int closed = data.Where(x => x.Status == (int)IssueStatusEnum.Close).Count();
 
-                return ApiResult<dynamic>.Success(new { open, closed, suspend });
+                return ApiResult<dynamic>.Success(new { open, closed });
             }
         }
     }   
